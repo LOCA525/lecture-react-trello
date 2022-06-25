e0fa2f87d8f108e713579b45aeeb3a108a6fa639
박준영 <pjy93525@gmail.com>
2022-06-25 20:52:10 +0900

    a

--- src/pages/BoardPage/BoardPage.js
+++ src/pages/BoardPage/BoardPage.js
@@ -27,3 +27,31 @@ const BoardPage = () => {
 };
 
 export default BoardPage;
+
+// [{
+//   >>>>trelloPage/1
+//   boardId: 1,
+//   boardTitle: firstBoard ,
+//   todoData: [id : 1
+//             todoTitle:안녕하세요!
+//             todoCard:{id:1 , 할일첫번째 },{id:1, 또다른 할일},{id:1, 세번째할일}
+
+//   ,         id :2
+//             TodoTitle:반갑다!
+//             todoCard:{id2, 할일이에요}]
+//   },
+
+//   {
+//     >>>>trelloPage/2
+//     boardId: 2,
+//     boardTitle: 두번째Board ,
+//     todoData: [id : 1
+//               todoTitle:오늘뭐할까요!
+//               todoCard:{id:1 , 밥먹기 },{id:1, 공부하기},{id:1, 자기}
+
+//     ,         id :2
+//               TodoTitle:낼은요!
+//               todoCard:{id2, 이거할거임ㅋ}]
+//     }
+
+// ];
